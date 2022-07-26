487a1781aeb8edb3906073edb54b4a0b4c87c9bf
IgorC-test <59960870+IgorC-test@users.noreply.github.com>
2022-07-26 14:14:50 +0200

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -2,6 +2,10 @@
 mkdir /usr/local/aw
 cd /usr/local/aw
 echo "downloading package"
+# currently TrueNAS 13.1 uses 12.3-RELEASE for its jails
+# if needed switch to awpst707-freebsd12.tgz and add
+# mv /usr/local/aw/binaries/FreeBSD/amd64/12 /usr/local/aw/binaries/FreeBSD/amd64/13
+# before executing start-server
 fetch http://p5-downloads.s3.amazonaws.com/awpst707-freebsd13.tgz
 echo "unpacking tar"
 tar -xvf /usr/local/aw/awpst*-freebsd13.tgz -C /usr/local/aw/
